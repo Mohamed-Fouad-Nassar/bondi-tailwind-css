aacea942b06c43c58d3da15970291243f0c7b25a
Mohamed Nassar <mohammednassar740@gmail.com>
2023-08-12 03:06:18 +0300

    upload design files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        mainBG: "#19283f",
+        secBG: "#33d1cc",
+        btnBG: "#ff3150",
+        btnHover: "#db3750",
+        transColor: "#eff7fa",
+        headColor: "#ffc501",
+        textColor: "#a8b9c0",
+      },
+    },
+  },
+  plugins: [],
+};
